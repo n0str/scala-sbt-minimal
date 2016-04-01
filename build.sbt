914d67618e123cc859e903e5980c3ad9129146ef
Alexander Menshikov <ntpcp@yandex.ru>
2016-04-01 20:06:31 +0300

    Add scalaj packet

--- build.sbt
+++ build.sbt
@@ -3,4 +3,5 @@ name := "stb-example"
 version := "1.0"
 
 scalaVersion := "2.11.8"
-    
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.2.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import scalaj.http._
+
 /**
   * Created by Nostr on 2016/04/01.
   */
@@ -5,6 +7,8 @@ object Main {
 
   def main(args: Array[String]) {
     println("Hello world")
+    val response: HttpResponse[String] = Http("https://ya.ru/").asString
+    println(response.body)
   }
 
 }
